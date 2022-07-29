885972372277fd0d80f5744fe5b6687ca82893b6
yogeshrajj <jagarwalyogesh@gmai.com>
2022-07-29 17:31:33 +0530

    adding controller and model

---
+++ WebApp/Controllers/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApp.Models;
+
+namespace WebApp.Controllers
+{
+    public class PlayerController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Player> lstPlayers = new List<Player>
+            {
+                new Player{ PId=1, PName="MSd", PAvg=60, PTeam="CSK"},
+                new Player{ PId=3, PName="MSd", PAvg=60, PTeam="CSK"},
+                new Player{ PId=5, PName="MSd", PAvg=60, PTeam="CSK"},
+                new Player{ PId=4, PName="MSd", PAvg=60, PTeam="CSK"},
+                new Player{ PId=2, PName="MSd", PAvg=60, PTeam="CSK"},
+
+            };
+            return View(lstPlayers);
+        }
+    }
+}
---
+++ WebApp/Models/Player.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApp.Models
+{
+    public class Player
+    {
+        public int PId { get; set; }
+        public string PName{ get; set; }
+        public string PTeam { get; set; }
+        public double PAvg { get; set; }
+    }
+}
